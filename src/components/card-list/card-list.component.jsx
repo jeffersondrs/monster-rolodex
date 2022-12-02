461604a046cabf23dd4afa717d52758a3536dc80
jeffersondrs <jeffersonduarte@id.uff.br>
2022-12-02 20:38:50 -0300

    fixing bug on cardlist component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,10 +2,9 @@ import "./card-list.style.css";
 import Card from "../card/card.component";
 
 const CardList = ({ monsters }) => (
-  <div className="card-list">
+  <div className='card-list'>
     {monsters.map((monster) => {
-      const { name, email, id } = monster;
-      return <Card monster={monster} />;
+      return <Card key={monster.id} monster={monster} />;
     })}
   </div>
 );
